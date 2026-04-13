da3db9259a300ffd8c6cee6845f1d9e85471d892
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-13 21:51:57 +0530

    added the question

--- src/main/java/Challenges/SalaryUnderBudget.java
+++ src/main/java/Challenges/SalaryUnderBudget.java
@@ -1,5 +1,18 @@
 package Challenges;
 
+//So The Question is that
+// Given an Array of salary of the employees
+// The assigned Budget is fixed for all the employees
+// So the sum of the salary of all the employee is fixed.
+//So Now We have to promote a employee and raise his salary
+// but twist is we can't raise budget so we have to equally deduct the salary of rest employees.
+// Or when we have to demote the employee we have to deduct his salary and equally raise the
+// other employee salary ;
+
+// Note  :  if you face a situation where you got exceptional case like you can't equally add or
+// deduct the salary from the array , (its when you face decimal places that can exceed the budget
+// or reduce the budget then add or duduct the salary only first index salary to make it balance
+//  the budget is fixed and can't change )
 public class SalaryUnderBudget {
     public static void main(String[] args) {
 

21f2b702cca3bc76430fea2fe3b41d44fb36d0cd
Ajay-Pandit-1293 <ajaypandit1293@gmail.com>
2026-04-30 21:26:48 +0530

    aadha adhura code

--- src/main/java/Challenges/RandomMoneyTransfer.java
+++ src/main/java/Challenges/RandomMoneyTransfer.java
@@ -1,4 +1,15 @@
 package Challenges;
 
+import java.util.Random;
+
+// I will complete it later because i am sleepy now
+
 public class RandomMoneyTransfer {
+    public static void main(String[] args) {
+        int a = 5000;
+        int b=5000;
+        Random random = new Random();
+        int num = random.nextInt(a);
+        System.out.println(num);
+    }
 }
